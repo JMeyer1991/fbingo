aff98eddd959886567b8e61288b438d4cee020b5
Jordan Meyer <jordanmeyer1991@gmail.com>
2024-12-14 21:18:49 -0500

    set bingo balls as single static array reduced code in gen_card() for more efficient looping

--- fbingo.f90
+++ fbingo.f90
@@ -9,6 +9,14 @@ module fbingo
                 logical, dimension(5, 5) :: dab
         end type card
 
+        integer, dimension(15, 5) :: balls
+
+        data balls / 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, &
+                16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, &
+                31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, &
+                46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, &
+                61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75 /
+
 contains
         function rand15() result(x)
                 ! function for generating random number between 1 and 15
@@ -32,78 +40,31 @@ contains
                 ! candidate integer for space being filled
                 integer :: x
 
-                ! row index
-                integer :: i
+                ! loop indices
+                integer :: i, j
 
-                ! variables specifying values for each column
-                integer, dimension(15) :: colb, coli, coln, colg, colo
-
-                ! set possible values for each column
-                colb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-                coli = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, &
-                        29, 30]
-                coln = [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, &
-                        44, 45]
-                colg = [46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, &
-                        59, 60]
-                colo = [61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, &
-                        74, 75]
-
+                ! assign numbers to card face
                 do i = 1, 5
-                        do while (.true.)
-                                x = rand15()
-                                if (any(temp_card%face(:, 1) == colb(x))) then
-                                        cycle
-                                else
-                                        temp_card%face(i, 1) = colb(x)
-                                        exit
-                                end if
-                        end do
-                        do while (.true.)
-                                x = rand15()
-                                if (any(temp_card%face(:, 2) == coli(x))) then
-                                        cycle
-                                else
-                                        temp_card%face(i, 2) = coli(x)
-                                        exit
-                                end if
-                        end do
-                        do while (.true.)
-                                if (i == 3) then
-                                        temp_card%face(i, 3) = 0
-                                        exit
-                                end if
-                                x = rand15()
-                                if (any(temp_card%face(:, 3) == coln(x))) then
-                                        cycle
-                                else
-                                        temp_card%face(i, 3) = coln(x)
-                                        exit
-                                end if
-                        end do
-                        do while (.true.)
-                                x = rand15()
-                                if (any(temp_card%face(:, 4) == colg(x))) then
-                                        cycle
-                                else
-                                        temp_card%face(i, 4) = colg(x)
-                                        exit
-                                end if
-                        end do
-                        do while (.true.)
-                                x = rand15()
-                                if (any(temp_card%face(:, 5) == colo(x))) then
-                                        cycle
-                                else
-                                        temp_card%face(i, 5) = colo(x)
-                                        exit
-                                end if
+                        do j = 1, 5
+                                do while (.true.)
+                                        x = balls(rand15(), j)
+                                        if (any(temp_card%face(:, j) == x)) then
+                                                cycle
+                                        else
+                                                temp_card%face(i, j) = x 
+                                                exit
+                                        end if
+                                end do
                         end do
                 end do
+
+                ! set free space to 0
+                temp_card%face(3, 3) = 0
 
                 ! set all dabs to 0
                 temp_card%dab(:, :) = .false.
 
+                ! return generated card value
                 new_card = temp_card
         end function gen_card
 
